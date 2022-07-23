a26491ec41829266f2802dd1aaf89de4d7fd42b7
Marcelo <marcelohbpereira15@gmail.com>
2022-07-23 13:22:53 -0300

    refactor: Clean Code, aplicando SOLID

--- src/App.js
+++ src/App.js
@@ -1,95 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import './style.css';
 
-
-let timer = null
-let min = 24
-let sec = 60
-
-
 export default function App() {
-
-    const [timerNumber, setTimerNumber] = useState(25);
     const [timerConfig, setTimerConfig] = useState(25);
     const [breakTime, setBreakTime] = useState(0);
-    const [status, setStatus] = useState(true)
+
+    const [status, setStatus] = useState(true);
+    const [breakStatus, setBreakStatus] = useState(true);
+    const [isBreak, setIsBreak] = useState(false)
+
+    const [seconds, setSeconds] = useState(timerConfig * 60);
+    const layoutMinutes = Math.floor(seconds / 60);
+    const layoutSeconds = seconds % 60;
+
+    useEffect(() => {
+        if (seconds === 0) {
+            const message = !isBreak ?
+                `\n PAUSA PARA DESCANSO! 😴 \n Clique no Break  Time para início, tempo selecionado: ${breakTime} min` :
+                `\n FIM DO DESCANSO! 🤓 \n Selecione o Pomodoro Timer para reinicio`;
+
+            alert(message);
+            setIsBreak(state => !state);
+            return;
+        }
+
+        if (breakStatus) return;
+ 
+        setTimeout(() => { setSeconds(seg => seg - 1) }, 1000)
+    }, [seconds]);
+
 
     function startPomodoro() {
-        sec = 60
-        timer = setInterval(() => {
-            sec--;
-
-            if (sec === 0) {
-                sec = 59;
-                min--;
-            }
-
-            if (min === -1) {
-                min = 0;
-                sec = 0;
-
-                alert('\n' + 'PAUSA PARA DESCANSO ! 😴' + '\n' + 'Clique no Break  Time para início, tempo selecionado: ' + breakTime + ' min');
-                clearInterval(timer)
-                timer = null
-            }
-
-            let format = (min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec);
-            setTimerNumber(format);
-
-        }, 100)
+        setBreakStatus(false);
+        setSeconds(seg => seg - 1);
     }
-
 
     function saveTime() {
-        setTimerNumber(timerConfig);
+        setSeconds(timerConfig * 60)
         setStatus(true);
-        min = timerConfig - 1
     }
-
 
     function startBreakTime() {
-        sec = 60;
-        min = breakTime - 1
-        timer = setInterval(() => {
-            sec--;
-
-            if (sec === 0) {
-                sec = 59;
-                min--;
-            }
-
-            if (min === -1) {
-                min = 0;
-                sec = 0;
-
-                alert('\n' + 'FIM DO DESCANSO ! 🤓' + '\n' + 'Selecione o Pomodoro Timer para reinicio');
-                clearInterval(timer)
-                timer = null
-            }
-
-            let format = (min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec);
-            setTimerNumber(format);
-
-        }, 100)
+        setBreakStatus(false);
+        setSeconds(breakTime * 60);
     }
 
-
     function pause() {
-        if (timer != null) {
-            clearInterval(timer);
-            timer = null;
-        }
+        setBreakStatus(true);
     }
 
     function handleBreak(e) {
-        let valueBreak = e.target.value;
+        const valueBreak = e.target.value;
         setBreakTime(valueBreak);
     }
 
-
     function handlePomodoro(e) {
-        let valuePomodoro = e.target.value
+        const valuePomodoro = e.target.value
         setTimerConfig(valuePomodoro);
     }
 
@@ -97,12 +64,9 @@ export default function App() {
         setStatus(false)
     }
 
-
     return (
-
         <div className="container">
             <Header />
-
             {
                 status ? <button className="buttonConfig" onClick={setConfig}>Settings ⚙️</button> :
 
@@ -116,20 +80,19 @@ export default function App() {
                         <input className="input" type='number' name='number' value={breakTime} onChange={handleBreak} />
 
                         <button className="buttonOk" onClick={saveTime}>Ok</button>
-
                     </div>
             }
 
-
             <img src={require('./assets/crono.png')} className="img" />
-            <a className="timer"> {timerNumber}</a>
+            <a className="timer"> {layoutMinutes.toString().padStart(2, '0')}:{layoutSeconds.toString().padStart(2, '0')}</a>
 
             <div className="buttonArea">
                 <button className="button" onClick={pause}>PAUSE</button>
                 <button className="buttonStart" onClick={startPomodoro}>START</button>
-                <button className="button" onClick={startBreakTime}>BREAK TIME</button>
+                {
+                    isBreak && <button className="button" onClick={startBreakTime}>BREAK TIME</button>
+                }
             </div>
-
         </div>
     )
 }
